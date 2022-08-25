7b63ff2483416590385967627d2ac8e3ef9b19ab
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2022-08-25 13:56:34 -0400

    Agregar archivos de proyecto.

---
+++ AplicacionInteresante/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AplicacionInteresante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
