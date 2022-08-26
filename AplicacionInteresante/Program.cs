2345f12bc4f7054c05cecebd5da5037097c29951
rusokverse <rusokverse@MACBOOKPRO>
2022-08-26 18:36:04 -0400

    Pequeño cambio

--- AplicacionInteresante/Program.cs
+++ AplicacionInteresante/Program.cs
@@ -14,7 +14,7 @@ namespace AplicacionInteresante
             PepeTron maquina = new PepeTron();
             maquina.CargarPrograma();
             maquina.VolcarMemoria();
-            
+            maquina.EjecutarPrograma();
 
             
         }
